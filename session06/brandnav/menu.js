62a77a15c1c1cab4de4d5cc0fafef9acf88ad5ca
jackisol <60629490+jackisol@users.noreply.github.com>
2021-11-22 01:27:59 -0500

    Adding Media Queries + Hamburger Menu

---
+++ session06/brandnav/menu.js
@@ -0,0 +1,10 @@
+function showMenu() {
+    document.getElementById('menuoverlay').style.display = 'block';
+    document.getElementById('page').style.display = 'none';
+}
+
+function hideMenu() {
+    document.getElementById('menuoverlay').style.display = 'none';
+    document.getElementById('page').style.display = 'block';
+}
+
