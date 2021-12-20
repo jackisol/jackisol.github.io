b1af4460eeb310af2111a7276e1e7ac3cdf65edd
jackisol <60629490+jackisol@users.noreply.github.com>
2021-12-19 22:52:57 -0500

    Adding Final Assignment

---
+++ session09/final/js/menu.js
@@ -0,0 +1,10 @@
+function showMenu() {
+    document.getElementById('menuoverlay').style.display = 'block';
+    document.getElementById('mainpage').style.display = 'none';
+}
+
+function hideMenu() {
+    document.getElementById('menuoverlay').style.display = 'none';
+    document.getElementById('mainpage').style.display = 'block';
+}
+
